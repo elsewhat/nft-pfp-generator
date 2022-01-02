4a109836de42c4ca409064095d2029e0020b54c7
Dagfinn Parnas <dagfinn.parnas@gmail.com>
2022-01-02 11:24:20 +0100

    Read traits dir

--- main.py
+++ main.py
@@ -17,19 +17,28 @@ class TraitGroup:
         self.traitsWeights = []
 
     def addTrait(self,trait):
-        self.traits.push(trait)
-        self.traitsWeights.push(trait.weight)
+        self.traits.append(trait)
+        self.traitsWeights.append(trait.weight)
                 
 
 
 def main():
     #wallet = sys.argv[0]
+
+    traitGroups = {}
+
     for path, subdirs, files in os.walk('traits/'):
-        for traitGroup in subdirs:
-            print(traitGroup)
-            for path2, subdirs2, traitFiles in os.walk(os.path.join('traits/',traitGroup)):
+        for traitGroupDir in subdirs:
+            print(traitGroupDir)
+            traitGroup = TraitGroup(traitGroupDir)
+
+            for path2, subdirs2, traitFiles in os.walk(os.path.join('traits/',traitGroupDir)):
                 for traitFile in traitFiles:
-                    print(os.path.join(path, traitFile))
+                    #print(os.path.join(path, traitFile))
+                    traitGroup.addTrait(Trait(traitFile,traitFile,1))
+
+            traitGroups[traitGroupDir]= traitGroup
+
 
         #for name in files:
             #print(os.path.join(path, name))
